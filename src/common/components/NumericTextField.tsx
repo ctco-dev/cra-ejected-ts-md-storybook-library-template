81d34b29d874786dac6d259b9902d1f9971fb532
trioletas <olegs.gabrusjonoks@gmail.com>
2017-04-12 20:30:52 +0300

    Unit test

--- src/common/components/NumericTextField.tsx
+++ src/common/components/NumericTextField.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import TextField from 'material-ui/TextField';
 
 interface NumericTextFieldProps extends __MaterialUI.TextFieldProps {
-  onChange: (event: any, value: string) => void;
+  onChange: (event: React.FormEvent<{}>, value: string) => void;
 }
 
-class NumericTextField extends React.Component<NumericTextFieldProps, any> {
+const normalizeInput = (value: string) => value.replace(/\D/g, '');
 
+class NumericTextField extends React.Component<NumericTextFieldProps, any> {
   render() {
     return (
       <TextField
@@ -17,13 +18,13 @@ class NumericTextField extends React.Component<NumericTextFieldProps, any> {
   }
 
   private handleChange = (event) => {
-    const value = this.normalizeInput(event.target.value);
+    const value = normalizeInput(event.target.value);
     this.props.onChange(event, value);
   }
-
-  private normalizeInput(value) {
-    return value.replace(/\D/g, '');
-  }
 }
+
+export {
+  normalizeInput,
+};
 
 export default NumericTextField;
