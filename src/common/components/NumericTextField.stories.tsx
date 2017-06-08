f2c6e4df75c1abd4847214c57ec00ea40dc313b0
trioletas <olegs.gabrusjonoks@gmail.com>
2017-06-08 22:11:54 +0300

    first stable version

--- src/common/components/NumericTextField.stories.tsx
+++ src/common/components/NumericTextField.stories.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 import NumericTextField from './NumericTextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 storiesOf('NumericTextField', module)
+  .addDecorator((story) => (
+    <MuiThemeProvider>
+      {story()}
+    </MuiThemeProvider>
+  ))
   .add('NumericTextField empty', () => (
     <NumericTextField id="test" onChange={action('changed')}/>
   ))
