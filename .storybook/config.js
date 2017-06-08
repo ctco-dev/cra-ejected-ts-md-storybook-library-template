2a4f7179fd25006e4508b961fb65e7ab1e18bd60
trioletas <olegs.gabrusjonoks@gmail.com>
2017-06-08 20:55:10 +0300

    New Storybook addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {configure, addDecorator} from '@storybook/react';
+import centered from '@storybook/addon-centered';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const req = require.context('../src/', true, /.stories.tsx$/);
@@ -8,9 +9,12 @@ const loadStories = () => {
   req.keys().forEach((filename) => req(filename))
 };
 
+addDecorator(centered);
+
 addDecorator((story) => (
   <MuiThemeProvider>
     {story()}
   </MuiThemeProvider>
 ));
+
 configure(loadStories, module);
