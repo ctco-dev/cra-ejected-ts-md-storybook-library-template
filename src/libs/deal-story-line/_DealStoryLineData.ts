b5214062b72ddb24a7d0c74ea9e599dbd73eba7d
Yurgis Rudaks <yurgis.rudaks@ctco.lv>
2017-07-07 12:44:43 +0300

    Generate typings bundle with public typings only

---
+++ src/libs/deal-story-line/_DealStoryLineData.ts
@@ -0,0 +1,65 @@
+export interface DealStoryLineLayer {
+    attachment?: number;
+    cover?: number;
+    frequency?: number;
+    loss?: number;
+}
+
+export interface DealStoryLineDataItem {
+    currency: string;
+    displayName: string;
+    layers: DealStoryLineLayer[];
+    ratingId: number;
+}
+
+export interface DealStoryLinePreparedDataItem extends DealStoryLineDataItem {
+    class: string;
+    end: number;
+    start: number;
+}
+
+const getBarClassName = (value: number, cumulative: number): string => {
+    if (cumulative === 0) {
+        return 'DealStoryLine__bar--base';
+    }
+
+    return (value >= cumulative) ? 'DealStoryLine__bar--positive' : 'DealStoryLine__bar--negative';
+};
+
+const getStartValue = (cumulative: number, endValue: number): number => {
+    return endValue === cumulative ? cumulative * 0.99 : cumulative;
+};
+
+const getEndValue = (itemValue: number, cumulative: number): number => {
+    return cumulative === 0 ? itemValue : cumulative - (cumulative - itemValue);
+};
+
+export const prepareData = (data: DealStoryLineDataItem[], layer: number = 0, value: string = 'loss'): DealStoryLinePreparedDataItem[] => {
+    const preparedData: DealStoryLinePreparedDataItem[] = [];
+
+    let cumulative: number = 0;
+    data.forEach(dataItem => {
+        const itemValue = dataItem.layers[layer][value];
+        if (!itemValue) {
+            return;
+        }
+
+        const endValue: number = getEndValue(itemValue, cumulative);
+
+        const preparedDataItem: DealStoryLinePreparedDataItem = {
+            ...dataItem,
+            class: getBarClassName(endValue, cumulative),
+            end: endValue,
+            start: getStartValue(cumulative, endValue),
+        };
+
+        preparedData.push(preparedDataItem);
+        cumulative = endValue;
+    });
+
+    // Override classname and start value for the last bar
+    preparedData[preparedData.length - 1].class = 'DealStoryLine__bar--total';
+    preparedData[preparedData.length - 1].start = 0;
+
+    return preparedData;
+};
